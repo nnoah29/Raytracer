6f79b3869a1bfe50cf32b36d94a588b836a675cf
nnoah29 <noah.toffa@epitech.eu>
2025-04-28 13:08:13 +0100

    cmake configuration

--- app/main.cpp
+++ app/main.cpp
@@ -9,6 +9,7 @@
 **         |___/ 
 */
 
+#include <iostream>
 /// 1. La caméra génère un rayon → (x, y)
 /// 2. Le rayon touche une primitive ? → Oui ? → continue
 /// 3. Je regarde où ça touche et quelle est la normale
@@ -21,7 +22,9 @@
 ///
 int main(int argc, char *argv[])
 {
-
+    std::cout << "Hello World!" << std::endl;
+    std::cout << argc << std::endl;
+    std::cout << argv[0] << std::endl;
 }
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
