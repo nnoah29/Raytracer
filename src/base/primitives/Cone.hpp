ff16ede457ef5c4fd6fe43875f6a3490b4dcea58
AGANI Hermes <hermes.agani@epitech.eu>
2025-05-15 20:46:13 +0200

    -cone primitive

---
+++ src/base/primitives/Cone.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** B-OOP-400-COT-4-1-raytracer-noah.toffa
+** File description:
+** Cone.hpp
+*/
+
+#ifndef CONE_HPP
+#define CONE_HPP
+
+#include "APrimitive.hpp"
+
+class Cone final: public APrimitive {
+public:
+    Cone(dataPrimitive& data, const std::string& name);
+
+    bool hit(const Ray& ray, float t_min, float t_max, PointOfImpact& p) const override;
+};
+
+#endif // CONE_HPP
